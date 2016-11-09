852b75fc14818c25fda0c2928882ec5c37fdada1
Douglas Silva <douglas.f.silva@accenture.com>
2016-11-08 22:20:01 -0200

    fix

--- sample-for-git/src/br/com/cielo/test/git/Authorizer.java
+++ sample-for-git/src/br/com/cielo/test/git/Authorizer.java
@@ -2,7 +2,7 @@ package br.com.cielo.test.git;
 
 import java.util.Date;
 
-// R1
+// R1 - fixed
 public class Authorizer {
 	public boolean authorize(Transaction t) {
 		if (t.getValue() < 120 && t.getTimestamp().compareTo(new Date()) < 0) {
